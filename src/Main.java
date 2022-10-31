73369231e81f4f09d788c91d91a829cbad3e174e
IrenaDemchenkova <iren.demchenkova@gmail.com>
2022-10-31 09:50:31 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static final String BASE_PATH = "C:\\Games\\savegames\\";
 
     public static GameProgress createGameProgress() {
-        GameProgress gameProgress = new GameProgress(
-                new Random().nextInt(100), //health
-                new Random().nextInt(50), //weapon
-                new Random().nextInt(100), // level
+        return new GameProgress(
+                new Random().nextInt(100), // health
+                new Random().nextInt(50), // weapon
+                new Random().nextInt(100), // lBASE_PATHevel
                 new Random().nextDouble(1000) // distance
         );
-        return gameProgress;
     }
 
     public static void saveGameProgress(GameProgress gameProgress, String filename) {
@@ -25,12 +26,58 @@ public class Main {
         }
     }
 
-        public static void main(String[] args) {
+    public static File[] listDirectoryFiles(String directoryPath) {
+        File saveGamesFolder = new File(directoryPath);
+        File[] saveGamesFiles = saveGamesFolder.listFiles();
+        if (saveGamesFiles == null) {
+            System.out.println("Directory with saved games doesn't exist.");
+            return null;
+        }
+        return saveGamesFiles;
+    }
+
+    public static void zipFiles(String zipFilePath, List<String> filesPaths) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(zipFilePath))) {
+            for (String filePath : filesPaths) {
+                // put file into the archive
+                ZipEntry zipEntry = new ZipEntry(filePath);
+                zout.putNextEntry(zipEntry);
+
+                // write some data into current file
+                try (FileInputStream currentFile = new FileInputStream(BASE_PATH + filePath)) {
+                    zout.write(currentFile.readAllBytes());
+                } catch (IOException e) {
+                    System.out.println("Unable to write data: " + e.getMessage());
+                }
+            }
+            zout.closeEntry();
+        } catch (Exception e) {
+            System.out.println("Unable to ZIP: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
         GameProgress gameProgress1 = createGameProgress();
         GameProgress gameProgress2 = createGameProgress();
         GameProgress gameProgress3 = createGameProgress();
         saveGameProgress(gameProgress1, "save1");
         saveGameProgress(gameProgress2, "save2");
         saveGameProgress(gameProgress3, "save3");
+
+        File[] saveGamesFiles = listDirectoryFiles(BASE_PATH);
+        if (saveGamesFiles == null) return;
+
+        ArrayList<String> saveGamesFilesPaths = new ArrayList<>();
+        for (File file : saveGamesFiles) saveGamesFilesPaths.add(file.getName());
+
+        zipFiles(BASE_PATH + "\\zipped.zip", saveGamesFilesPaths);
+
+        for (String saveGameFilePath : saveGamesFilesPaths) {
+            File saveGameFile = new File(BASE_PATH + saveGameFilePath);
+            if (saveGameFile.delete())
+                System.out.println("Deleted " + saveGameFilePath);
+            else
+                System.out.println("Unable to delete " + saveGameFilePath);
+        }
     }
 }
